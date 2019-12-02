08edfff24cef429addd25714bc366db4ecccf0a9
alex-milevsky <58426139+alex-milevsky@users.noreply.github.com>
2019-12-02 14:47:42 +0200

    Add files via upload

---
+++ hotline/src/test/java/testsite.java
@@ -0,0 +1,13 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class testsite {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();driver.get("https://www.olx.ua/");
+        driver.findElement(By.id("headerSearch")).sendKeys("телевизор" + Keys.ENTER);
+    }
+}
