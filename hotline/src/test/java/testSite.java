bfa71b8677b7523418d81a459e0bc42ceab08bc5
alex-milevsky <QWEasd1985->
2019-12-02 20:13:25 +0200

    update main

--- hotline/src/test/java/testSite.java
+++ hotline/src/test/java/testSite.java
@@ -4,10 +4,10 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class testsite {
+public class testSite {
     public static void main(String[] args) {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();driver.get("https://www.olx.ua/");
-        driver.findElement(By.id("headerSearch")).sendKeys("телевизор" + Keys.ENTER);
+        driver.findElement(By.id("headerSearch")).sendKeys("телевизор akai" + Keys.ENTER);
     }
 }
